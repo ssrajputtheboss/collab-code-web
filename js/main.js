cdb6a6309ee084eaf3fb3419cb66f12cc891354a
ssrajputtheboss <62293640+ssrajputtheboss@users.noreply.github.com>
2025-06-24 21:40:29 +0530

    codeio simple

--- js/main.js
+++ js/main.js
@@ -1,54 +1,91 @@
-const HOST = 'https://collab-code-api.herokuapp.com/';
-let socket = io(HOST, {
-  extraHeaders: {
-    authorization:
-      'eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOiJhcGkiLCJpYXQiOjE2NTY5MDk3MTcsImV4cCI6MTY1NzA4MjUxN30.7lDpIDlwkPCCpAzsx2AsWRXD22gDxMBLAGTJbc7v1fc'
-  }
-});
-let fileList = [];
-let activeIndex = -1;
-let token = '';
-let userList = [];
-let roomname = '';
-let newFile = '';
-let isEditorOpen = false;
-let lastOutput = '';
-let me = '';
-let isDisconnected = false;
-const logger = new Logger();
-logger.isActive = false;
 
-function log() {
-  console.log({
-    fileList: fileList,
-    activeIndex: activeIndex,
-    token: token,
-    userList: userList,
-    roomname: roomname,
-    newFile: newFile
-  });
-}
 
-function tabLogger() {
-  let i = 0;
-  while (true) {
-    try {
-      const ele = document.getElementById('tab' + i);
-      console.log(ele);
-      if (!ele) break;
-      ++i;
-    } catch (err) {
+const HOST = 'https://codeio.xuitz.solutions/v1';
+let authToken = null;
+let runResponse = '';
+let running = false;
+
+
+function setLanguageEditor() {
+  let runtime = document.getElementById('runtime').value;
+  let mode = 'x-python';
+  switch(runtime){
+    case 'python3' :
+      mode = 'x-python';
       break;
+    case 'c++':
+      mode = 'x-c++src'
+      break;
+    default:
+      mode = 'text'
+  }
+  editor.setOption('mode', 'text/' + mode);
+
+}
+
+
+function login(){
+
+  fetch(HOST + '/testlogin' , {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+  }).then(res => {
+    if(res.status == 200){
+      let body = JSON.parse(res.body)
+      authToken = body.token;
+      hideLogin()
+      showEditor()
+    }else{
+      // service likely unavailable
     }
+  }).catch(err => {
+    console.log(err)
+  })
+
+}
+
+function run(code, runtime , input){
+  if (runtime != "python3" || runtime != "c++"){
+    console.log('runtime not supported')
+  }else{
+    fetch(HOST + '/run', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      script : code , 
+      runtime ,
+      input 
+    })
+  }).then(res => {
+    if(res.status == 200){
+      runResponse = res.body
+    }
+  })
   }
 }
 
-window.onbeforeunload = function (e) {
-  if (!roomname) return;
-  const lv = confirm('reloading will bring you to login page , are you sure to leave?');
-  if (lv) {
-    leave();
-  } else {
-    e.preventDefault();
-  }
-};
+function throttledRunner(){
+  if(running)alert('a request is already running!')
+  running = true;
+  let code = editor.getValue(),
+    runtime = document.getElementById('runtime').value,
+    input = document.getElementById('stdin').value;
+  run(code , runtime , input)
+  running = false;
+}
+
+// window.onbeforeunload = function (e) {
+//   if (!roomname) return;
+//   const lv = confirm('reloading will bring you to login page , are you sure to leave?');
+//   if (lv) {
+//     leave();
+//   } else {
+//     e.preventDefault();
+//   }
+// };
